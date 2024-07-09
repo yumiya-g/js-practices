555959c68576a27ec9bc0c0d3edff856b689ad4f
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2024-07-10 08:39:10 +0900

    Fix: `Days.js`の日付を取得する処理を、配列に変更しました
    - https://day.js.org/docs/en/parse/array

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -12,7 +12,7 @@ const year = yearMonthValue[0] || dayjs(today).year();
 const month = yearMonthValue[1] || dayjs(today).month() + 1;
 const firstDay = dayjs(new Date(year, month, 1)).date();
 const lastDay = dayjs(new Date(year, month, 0)).date();
-const firstDayOfWeek = dayjs(`"${year}-${month}-${firstDay}"`).day();
+const firstDayOfWeek = dayjs([year, month, firstDay]).day();
 
 console.log(`${" ".repeat(7)}${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
@@ -28,7 +28,7 @@ const createFirstDayToLastday = (lastDay) => {
 const addLineBreaksToDays = () => {
   const allDays = createFirstDayToLastday(lastDay);
   const dayWithLineBreaks = allDays.map((day) => {
-    const dayOfWeek = dayjs(`"${year}-${month}-${day}"`).day();
+    const dayOfWeek = dayjs([year, month, day]).day();
     return dayOfWeek === 6 ? `${day}\n` : `${day} `;
   });
   return dayWithLineBreaks;
