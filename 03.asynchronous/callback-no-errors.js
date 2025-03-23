1e4c8315fc23f0412610c3e8eb1c917d3ae1ccba
yumiya <52166120+yumiya-g@users.noreply.github.com>
2025-03-24 08:37:11 +0900

    Fix: SQL文を修正して、`INSERT`文と`SELECT`文を変数に代入しました
    - `IF NOT EXISTS`を削除
    - SQL文を大文字に修正

--- 03.asynchronous/callback-no-errors.js
+++ 03.asynchronous/callback-no-errors.js
@@ -2,47 +2,42 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-const createTableQuery = `CREATE TABLE IF NOT EXISTS books (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL UNIQUE
-  )`;
+const createTableQuery = `CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)`;
+const insertTableQuery = `INSERT INTO books (title) VALUES(?)`;
+const selectTableQuery = `SELECT * FROM books`;
 
 db.run(createTableQuery, function (err) {
   if (err) {
     console.log(err.message);
   } else {
     db.run(
-      "insert into books(title) values(?)",
+      insertTableQuery,
       "スラスラ読める JavaScriptふりがなプログラミング",
       function (err) {
         if (err) {
           console.log(err.message);
         } else {
           console.log(`ID: ${this.lastID}`);
-          db.run(
-            "insert into books(title) values(?)",
-            "初めてのJavaScript",
-            function (err) {
-              if (err) {
-                console.log(err.message);
-              } else {
-                console.log(`ID: ${this.lastID}`);
-                db.each(
-                  "select * from books",
-                  (err, row) => {
-                    console.log(`ID: ${row.id}, タイトル: ${row.title}`);
-                  },
-                  function (err) {
-                    if (err) {
-                      console.log(err.message);
-                    } else {
-                      db.close();
-                    }
-                  },
-                );
-              }
-            },
-          );
+          db.run(insertTableQuery, "初めてのJavaScript", function (err) {
+            if (err) {
+              console.log(err.message);
+            } else {
+              console.log(`ID: ${this.lastID}`);
+              db.each(
+                selectTableQuery,
+                (err, row) => {
+                  console.log(`ID: ${row.id}, タイトル: ${row.title}`);
+                },
+                function (err) {
+                  if (err) {
+                    console.log(err.message);
+                  } else {
+                    db.close();
+                  }
+                },
+              );
+            }
+          });
         }
       },
     );
