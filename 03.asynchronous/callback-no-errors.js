d010fa52f214c60042c866a0c6d24b17d150af8d
yumiya <52166120+yumiya-g@users.noreply.github.com>
2025-03-30 15:43:24 +0900

    Fix: コールバック関数を修正しました
    - `import`順を見直し
    - エラーありの処理を1ファイルにまとめる
      - `timers.setTimeout()`で待ち時間を追加
      - エラーなし処理用のデータベースを作成
      - SQL文を変数に代入

--- 03.asynchronous/callback-no-errors.js
+++ 03.asynchronous/callback-no-errors.js
@@ -1,29 +1,62 @@
+import timers from "timers/promises";
 import sqlite3 from "sqlite3";
 
-const db = new sqlite3.Database(":memory:");
+const valid_db = new sqlite3.Database(":memory:");
+const invalid_db = new sqlite3.Database(":memory:");
 
 const createTableQuery = `CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)`;
 const insertTableQuery = `INSERT INTO books (title) VALUES(?)`;
 const selectTableQuery = `SELECT * FROM books`;
+const insertTableWrongQuery = `INSERT INTO bookss (title) VALUES(?)`;
+const selectTableWrongQuery = `SELECT hoge FROM bbbooks`;
 
-db.run(createTableQuery, function () {
-  db.run(
+valid_db.run(createTableQuery, function () {
+  valid_db.run(
     insertTableQuery,
     "スラスラ読める JavaScriptふりがなプログラミング",
     function () {
       console.log(`ID: ${this.lastID}`);
-      db.run(insertTableQuery, "初めてのJavaScript", function () {
+      valid_db;
+      valid_db.run(insertTableQuery, "初めてのJavaScript", function () {
         console.log(`ID: ${this.lastID}`);
-        db.each(
-          selectTableQuery,
-          (_err, row) => {
-            console.log(`ID: ${row.id}, タイトル: ${row.title}`);
-          },
-          () => {
-            db.close();
-          },
-        );
+        valid_db.each(selectTableQuery, (_err, row) => {
+          console.log(`ID: ${row.id}, タイトル: ${row.title}`);
+        });
       });
     },
   );
 });
+
+valid_db.close();
+
+await timers.setTimeout(100);
+
+invalid_db.run(createTableQuery, () => {
+  invalid_db.run(
+    insertTableWrongQuery,
+    "スラスラ読める JavaScriptふりがなプログラミング",
+    (err) => {
+      if (err) {
+        console.log(err.message);
+        invalid_db.run(insertTableQuery, null, (err) => {
+          if (err) {
+            console.log(err.message);
+            invalid_db.each(selectTableWrongQuery, (err, _row) => {
+              if (err) {
+                console.log(err.message);
+              } else {
+                console.log(_row);
+              }
+            });
+          } else {
+            console.log("エラーなし");
+          }
+        });
+      } else {
+        console.log("エラーなし");
+      }
+    },
+  );
+});
+
+invalid_db.close();
