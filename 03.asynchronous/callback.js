c59584c4cdcd41a34bc32574aedaf2a98bb1341c
yumiya <52166120+yumiya-g@users.noreply.github.com>
2025-04-19 06:26:19 +0900

    Fix: コールバック関数のデータベースの接続処理を修正しました

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -1,52 +1,58 @@
 import timers from "timers/promises";
 import sqlite3 from "sqlite3";
 
-const valid_db = new sqlite3.Database(":memory:");
-const invalid_db = new sqlite3.Database(":memory:");
-
+let db = new sqlite3.Database(":memory:");
 const createTableQuery = `CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)`;
 const insertTableQuery = `INSERT INTO books (title) VALUES(?)`;
 const selectTableQuery = `SELECT * FROM books`;
 const insertTableWrongQuery = `INSERT INTO bookss (title) VALUES(?)`;
 const selectTableWrongQuery = `SELECT hoge FROM bbbooks`;
 
-valid_db.run(createTableQuery, function () {
-  valid_db.run(
+db.run(createTableQuery, function () {
+  db.run(
     insertTableQuery,
     "スラスラ読める JavaScriptふりがなプログラミング",
     function () {
       console.log(`ID: ${this.lastID}`);
-      valid_db.run(insertTableQuery, "初めてのJavaScript", function () {
+      db.run(insertTableQuery, "初めてのJavaScript", function () {
         console.log(`ID: ${this.lastID}`);
-        valid_db.each(selectTableQuery, (_err, row) => {
-          console.log(`ID: ${row.id}, タイトル: ${row.title}`);
-        });
+        db.each(
+          selectTableQuery,
+          (_err, row) => {
+            console.log(`ID: ${row.id}, タイトル: ${row.title}`);
+          },
+          () => db.close(),
+        );
       });
     },
   );
 });
 
-valid_db.close();
-
 await timers.setTimeout(100);
 
-invalid_db.run(createTableQuery, () => {
-  invalid_db.run(
+db = new sqlite3.Database(":memory:");
+
+db.run(createTableQuery, () => {
+  db.run(
     insertTableWrongQuery,
     "スラスラ読める JavaScriptふりがなプログラミング",
     (err) => {
       if (err) {
         console.log(err.message);
-        invalid_db.run(insertTableQuery, null, (err) => {
+        db.run(insertTableQuery, null, (err) => {
           if (err) {
             console.log(err.message);
-            invalid_db.each(selectTableWrongQuery, (err, _row) => {
-              if (err) {
-                console.log(err.message);
-              } else {
-                console.log(_row);
-              }
-            });
+            db.each(
+              selectTableWrongQuery,
+              (err, _row) => {
+                if (err) {
+                  console.log(err.message);
+                } else {
+                  console.log(_row);
+                }
+              },
+              () => db.close(),
+            );
           } else {
             console.log("エラーなし");
           }
@@ -57,5 +63,3 @@ invalid_db.run(createTableQuery, () => {
     },
   );
 });
-
-invalid_db.close();
