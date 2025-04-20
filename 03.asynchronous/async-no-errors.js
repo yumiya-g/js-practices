573109da3ceafd49fa056241b25d1417c13ec891
yumiya <52166120+yumiya-g@users.noreply.github.com>
2025-04-20 15:56:39 +0900

    Fix: Async/awaitエラーなしの修正を行いました
    - resisterBooks関数を使用しない
    - データベース作成とデータ追加、データベースを閉じる関数を作成

--- 03.asynchronous/async-no-errors.js
+++ 03.asynchronous/async-no-errors.js
@@ -1,33 +1,62 @@
-import * as noError from "./registerBooks.js";
+import timers from "timers/promises";
+import sqlite3 from "sqlite3";
 
-async function AsyncNoError() {
-  await noError.registerBooks(noError.createTableQuery);
-  await noError.registerBooks(
-    "insert into books(title) values(?)",
-    "スラスラ読める JavaScriptふりがなプログラミング",
-  );
-  await noError.registerBooks(
-    "insert into books(title) values(?)",
-    "初めてのJavaScript",
-  );
-  await noError.registerBooks(
-    "insert into books(title) values(?)",
-    "JavaScript入門",
-  );
+let db = new sqlite3.Database(":memory:");
+const createTableQuery = `CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)`;
+const insertTableQuery = `INSERT INTO books (title) VALUES(?)`;
+const selectTableQuery = `SELECT * FROM books`;
+
+const promise = (db) => {
+  return new Promise((resolve) => {
+    db.run(createTableQuery, function () {
+      resolve();
+    });
+  });
+};
+
+const insertFirstBook = (db) =>
+  new Promise((resolve) => {
+    db.run(
+      insertTableQuery,
+      "スラスラ読める JavaScriptふりがなプログラミング",
+      function () {
+        resolve({ props: this, db });
+      },
+    );
+  });
+
+const insertSecondBook = ({ props, db }) =>
+  new Promise((resolve) => {
+    db.run(insertTableQuery, "初めてのJavaScript", function () {
+      console.log(`ID: ${props.lastID}`);
+      resolve({ props: this, db });
+    });
+  });
+
+const displayBooks = ({ props, db }) =>
+  new Promise((resolve) => {
+    console.log(`ID: ${props.lastID}`);
+    db.each(selectTableQuery, (_err, row) => {
+      console.log(`ID: ${row.id}, タイトル: ${row.title}`);
+    });
+    resolve();
+  });
+
+const closeDatabase = () =>
+  new Promise(() => {
+    db.close();
+  });
+
+async function asyncNoError() {
+  let props;
+  await promise(db);
+
+  props = await insertFirstBook(db);
+  props = await insertSecondBook(props);
+  await displayBooks(props);
+  await closeDatabase();
 }
 
-AsyncNoError().finally(() => {
-  noError.db.each(
-    "select * from books",
-    (err, row) => {
-      console.log(`ID: ${row.id}, タイトル: ${row.title}`);
-    },
-    function (err) {
-      if (err) {
-        console.log(err.message);
-      } else {
-        noError.db.close();
-      }
-    },
-  );
-});
+asyncNoError();
+
+await timers.setTimeout(100);
