99dfcd9d04465e268110c2a13d3fb2380fcd5b0e
yumiya <52166120+yumiya-g@users.noreply.github.com>
2025-04-29 15:14:56 +0900

    Fix: `node-sqlite3`の関数を`Promise`でラップする関数を作成しました

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -15,84 +15,91 @@ const promise = (db) =>
     });
   });
 
-promise(db)
-  .then(
-    (db) =>
-      new Promise((resolve) => {
-        db.run(
-          insertTableQuery,
-          "スラスラ読める JavaScriptふりがなプログラミング",
-          function () {
-            resolve({ props: this, db });
-          },
-        );
-      }),
-  )
-  .then(
-    ({ props, db }) =>
-      new Promise((resolve) => {
-        db.run(insertTableQuery, "初めてのJavaScript", function () {
-          console.log(`ID: ${props.lastID}`);
-          resolve({ props: this, db });
-        });
-      }),
-  )
-  .then(({ props, db }) => {
+const insertFirstBook = (db) =>
+  new Promise((resolve) =>
+    db.run(
+      insertTableQuery,
+      "スラスラ読める JavaScriptふりがなプログラミング",
+      function () {
+        resolve({ props: this, db });
+      },
+    ),
+  );
+
+const insertFirstBookError = (db) =>
+  new Promise((_, reject) =>
+    db.run(
+      insertTableWrongQuery,
+      "スラスラ読める JavaScriptふりがなプログラミング",
+      function (err) {
+        if (err) {
+          console.log(err.message);
+          reject(db);
+        } else {
+          _(this);
+        }
+      },
+    ),
+  );
+
+const insertSecondBook = ({ props, db }) =>
+  new Promise((resolve) =>
+    db.run(insertTableQuery, "初めてのJavaScript", function () {
+      console.log(`ID: ${props.lastID}`);
+      resolve({ props: this, db });
+    }),
+  );
+
+const insertSecondBookError = (db) =>
+  new Promise((_, reject) =>
+    db.run(insertTableQuery, null, function (err) {
+      if (err) {
+        console.log(err.message);
+        reject(db);
+      } else {
+        _(this);
+      }
+    }),
+  );
+
+const displayBooks = ({ props, db }) =>
+  new Promise((resolve) => {
     console.log(`ID: ${props.lastID}`);
-    return new Promise((resolve) => {
-      db.each(selectTableQuery, (_err, row) => {
+    db.each(
+      selectTableQuery,
+      (_err, row) => {
         console.log(`ID: ${row.id}, タイトル: ${row.title}`);
-      });
-      resolve();
-    });
-  })
-  .finally(() => new Promise(() => db.close()));
+      },
+      () => resolve(),
+    );
+  });
+
+const displayBooksError = (db) =>
+  new Promise((_, reject) =>
+    db.each(selectTableWrongQuery, (err, _row) => {
+      if (err) {
+        console.log(err.message);
+        reject();
+      } else {
+        console.log(`ID: ${_row.id}, タイトル: ${_row.title}`);
+        _(this);
+      }
+    }),
+  );
+
+const closeDatabase = () => new Promise(() => db.close());
+
+promise(db)
+  .then((db) => insertFirstBook(db))
+  .then((obj) => insertSecondBook(obj))
+  .then((obj) => displayBooks(obj))
+  .finally(() => closeDatabase());
 
 await timers.setTimeout(100);
 db = new sqlite3.Database(":memory:");
 
 promise(db)
-  .then(
-    (db) =>
-      new Promise((_resolve, reject) =>
-        db.run(
-          insertTableWrongQuery,
-          "スラスラ読める JavaScriptふりがなプログラミング",
-          function (err) {
-            if (err) {
-              console.log(err.message);
-              reject(db);
-            } else {
-              _resolve(this);
-            }
-          },
-        ),
-      ),
-  )
-  .catch(
-    (db) =>
-      new Promise((_resolve, reject) =>
-        db.run(insertTableQuery, null, (err) => {
-          if (err) {
-            console.log(err.message);
-            reject(db);
-          } else {
-            _resolve(this);
-          }
-        }),
-      ),
-  )
-  .catch(
-    (db) =>
-      new Promise((_resolve, reject) =>
-        db.each(selectTableWrongQuery, (err) => {
-          if (err) {
-            console.log(err.message);
-            reject();
-          } else {
-            _resolve(this);
-          }
-        }),
-      ),
-  )
-  .finally(() => new Promise(() => db.close()));
+  .then((db) => insertFirstBookError(db))
+  .catch((db) => insertSecondBookError(db))
+  .catch((db) => displayBooksError(db))
+  .finally(() => closeDatabase());
