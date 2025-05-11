eb8c1ea78d91f9d4d4b4c34fddc8eaabef255e71
yumiya <52166120+yumiya-g@users.noreply.github.com>
2025-05-11 10:45:26 +0900

    Fix: テーブルを削除するSQL文と、各変数の`export`宣言の方法を修正しました

--- 03.asynchronous/queries.js
+++ 03.asynchronous/queries.js
@@ -1,16 +1,7 @@
-const createTableQuery =
+export const createTableQuery =
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)";
-const insertTableQuery = "INSERT INTO books (title) VALUES (?)";
-const selectTableQuery = "SELECT * FROM books";
-const insertTableWrongQuery = "INSERT INTO bookss (title) VALUES (?)";
-const selectTableWrongQuery = "SELECT hoge FROM bbbooks";
-const deleteTableQuery = "DROP TABLE IF EXISTS books";
-
-export {
-  createTableQuery,
-  insertTableQuery,
-  selectTableQuery,
-  insertTableWrongQuery,
-  selectTableWrongQuery,
-  deleteTableQuery,
-};
+export const insertTableQuery = "INSERT INTO books (title) VALUES (?)";
+export const selectTableQuery = "SELECT * FROM books";
+export const insertTableWrongQuery = "INSERT INTO bookss (title) VALUES (?)";
+export const selectTableWrongQuery = "SELECT hoge FROM bbbooks";
+export const deleteTableQuery = "DROP TABLE books";
