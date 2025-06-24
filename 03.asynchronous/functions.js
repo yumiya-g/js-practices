b057e9c80d34e670b3609e5fa5aaa0e9adede6b9
yumiya <52166120+yumiya-g@users.noreply.github.com>
2025-06-25 08:35:40 +0900

    Update: Promiseの処理を修正しました
    - データベースの切断を`finally`メソッドで実行するよう修正
    - エラーありの処理で、コールバック関数を見直しエラーハンドリングを追加
    
    - Promiseでラップしている関数内の処理を外部関数に切り出し

--- 03.asynchronous/functions.js
+++ 03.asynchronous/functions.js
@@ -1,50 +1,67 @@
 import sqlite3 from "sqlite3";
-
 const createDB = () => new sqlite3.Database("database");
-
 export let db = createDB();
-
 export const recreateDB = () => {
   db = createDB();
 };
 
 export const promiseRun = (query, params = [], callback = null) =>
-  new Promise((resolve, _reject) => {
+  new Promise((resolve, reject) => {
     if (callback === null) {
-      callback = function () {
-        resolve(this);
-      };
+      callback = skipCallback(resolve);
     }
-
-    db.run(query, params, callback);
+    const wrappedCallback = executeCallbackWrapper(resolve, reject, callback);
+    db.run(query, params, wrappedCallback);
   });
+
+const skipCallback = (resolve) => {
+  return function () {
+    resolve(this);
+  };
+};
+
+const executeCallbackWrapper = (resolve, reject, callback) => {
+  return function (err) {
+    callback.call(this, err);
+    if (err) {
+      reject(err);
+    } else {
+      resolve(this);
+    }
+  };
+};
 
 export const promiseEach = (query, ...args) =>
-  new Promise((resolve, _reject) => {
-    let params = [];
-    let callback = null;
-    let complete = null;
-
-    if (typeof args[0] === "function") {
-      params = [];
-      callback = args[0];
-      complete = args[1];
-    } else {
-      params = args[0];
-      callback = args[1];
-      complete = args[2];
-    }
-
-    resolve(
-      db.each(
-        query,
-        params,
-        (_err, row) => {
-          callback(_err, row);
-        },
-        () => {
-          complete();
-        },
-      ),
-    );
+  new Promise((resolve) => {
+    const { params, callback, complete } = parseArguments(args);
+    const originComplete = complete;
+    const wrappedComplete = executeCompleteWrapper(originComplete, resolve);
+    db.each(query, params, callback, wrappedComplete);
   });
+
+const parseArguments = (args) => {
+  const isFirstArgFunction = typeof args[0] === "function";
+  if (isFirstArgFunction) {
+    return { params: [], callback: args[0], complete: args[1] };
+  } else {
+    return {
+      params: args[0],
+      callback: args[1],
+      complete: args[2],
+    };
+  }
+};
+
+const executeCompleteWrapper = (originComplete, resolve) => {
+  return function (err) {
+    if (typeof originComplete === "function") {
+      originComplete();
+    }
+    if (err) {
+      console.error(err.message);
+      resolve(err);
+    } else {
+      resolve(this);
+    }
+  };
+};
--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -23,17 +23,14 @@ promiseRun(createTableQuery)
   })
   .then(function (obj) {
     console.log(`ID: ${obj.lastID}`);
-    return promiseEach(
-      selectTableQuery,
-      (_err, row) => {
-        console.log(`ID: ${row.id}, タイトル: ${row.title}`);
-      },
-      () => {
-        db.run(deleteTableQuery, () => {
-          db.close();
-        });
-      },
-    );
+    return promiseEach(selectTableQuery, (_err, row) => {
+      console.log(`ID: ${row.id}, タイトル: ${row.title}`);
+    });
+  })
+  .finally(() => {
+    db.run(deleteTableQuery, () => {
+      db.close();
+    });
   });
 
 await timers.setTimeout(100);
@@ -43,25 +40,43 @@ recreateDB();
 promiseRun(createTableQuery)
   .then(function () {
     return promiseRun(
-      insertTableQuery,
+      insertTableWrongQuery,
       "スラスラ読める JavaScriptふりがなプログラミング",
-    );
-  })
-  .then(function (obj) {
-    console.log(`ID: ${obj.lastID}`);
-    return promiseRun(insertTableQuery, "初めてのJavaScript");
-  })
-  .then(function (obj) {
-    console.log(`ID: ${obj.lastID}`);
-    return promiseEach(
-      selectTableQuery,
-      (_err, row) => {
-        console.log(`ID: ${row.id}, タイトル: ${row.title}`);
-      },
-      () => {
-        db.run(deleteTableQuery, () => {
-          db.close();
-        });
+      function (err) {
+        if (err) {
+          console.error(err.message);
+        } else {
+          console.log(`ID: ${this.lastID}`);
+        }
       },
     );
+  })
+  .then(function () {
+    return promiseRun(insertTableQuery, "初めてのJavaScript", function () {
+      console.log(`ID: ${this.lastID}`);
+    });
+  })
+  .then(function () {
+    return promiseEach(selectTableQuery, (_err, row) => {
+      console.log(`ID: ${row.id}, タイトル: ${row.title}`);
+    });
+  })
+  .catch(function () {
+    return promiseRun(insertTableQuery, null, function (err) {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log(`ID: ${this.lastID}`);
+      }
+    });
+  })
+  .catch(function () {
+    return promiseEach(selectTableWrongQuery, (err) => {
+      console.error(err.message);
+    });
+  })
+  .finally(() => {
+    db.run(deleteTableQuery, () => {
+      db.close();
+    });
   });
