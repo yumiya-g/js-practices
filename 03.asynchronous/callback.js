7fab9ed1b5ea334be8ff2ee595b55bddb30943d5
yumiya <52166120+yumiya-g@users.noreply.github.com>
2025-07-11 14:42:00 +0900

    fix: データベース接続処理をコード内で定義し、実行する関数に引数として渡すよう修正しました
    - 外部ファイルに定義していたデータベース接続処理をコード内に定義して、変数`db`に代入
    - 「エラーあり」「エラーなし」の処理を実行する関数を定義し、引数に`db`を渡す

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -1,5 +1,5 @@
 import timers from "timers/promises";
-import { db, recreateDB } from "./functions.js";
+import sqlite3 from "sqlite3";
 import {
   createTableQuery,
   insertTableQuery,
@@ -9,65 +9,75 @@ import {
   deleteTableQuery,
 } from "./queries.js";
 
-db.run(createTableQuery, () => {
-  db.run(
-    insertTableQuery,
-    "スラスラ読める JavaScriptふりがなプログラミング",
-    function () {
-      console.log(`ID: ${this.lastID}`);
-      db.run(insertTableQuery, "初めてのJavaScript", function () {
+let db = new sqlite3.Database("database");
+
+const registerDatabase = (db) => {
+  db.run(createTableQuery, () => {
+    db.run(
+      insertTableQuery,
+      "スラスラ読める JavaScriptふりがなプログラミング",
+      function () {
         console.log(`ID: ${this.lastID}`);
-        db.each(
-          selectTableQuery,
-          (_err, row) => {
-            console.log(`ID: ${row.id}, タイトル: ${row.title}`);
-          },
-          () => {
-            db.run(deleteTableQuery, () => {
-              db.close();
-            });
-          },
-        );
-      });
-    },
-  );
-});
+        db.run(insertTableQuery, "初めてのJavaScript", function () {
+          console.log(`ID: ${this.lastID}`);
+          db.each(
+            selectTableQuery,
+            (_err, row) => {
+              console.log(`ID: ${row.id}, タイトル: ${row.title}`);
+            },
+            () => {
+              db.run(deleteTableQuery, () => {
+                db.close();
+              });
+            },
+          );
+        });
+      },
+    );
+  });
+};
+
+registerDatabase(db);
 
 await timers.setTimeout(100);
 
-recreateDB();
+db = new sqlite3.Database("database");
 
-db.run(createTableQuery, () => {
-  db.run(
-    insertTableWrongQuery,
-    "スラスラ読める JavaScriptふりがなプログラミング",
-    function (err) {
-      if (err) {
-        console.error(err.message);
-      } else {
-        console.log(`ID: ${this.lastID}`);
-      }
-      db.run(insertTableQuery, null, function (err) {
+const registerDatabaseWithErrors = (db) => {
+  db.run(createTableQuery, () => {
+    db.run(
+      insertTableWrongQuery,
+      "スラスラ読める JavaScriptふりがなプログラミング",
+      function (err) {
         if (err) {
           console.error(err.message);
         } else {
           console.log(`ID: ${this.lastID}`);
         }
-        db.each(
-          selectTableWrongQuery,
-          (_err, row) => {
-            console.log(`ID: ${row.id}, タイトル: ${row.title}`);
-          },
-          (err) => {
-            if (err) {
-              console.error(err.message);
-            }
-            db.run(deleteTableQuery, () => {
-              db.close();
-            });
-          },
-        );
-      });
-    },
-  );
-});
+        db.run(insertTableQuery, null, function (err) {
+          if (err) {
+            console.error(err.message);
+          } else {
+            console.log(`ID: ${this.lastID}`);
+          }
+          db.each(
+            selectTableWrongQuery,
+            (_err, row) => {
+              console.log(`ID: ${row.id}, タイトル: ${row.title}`);
+            },
+            (err) => {
+              if (err) {
+                console.error(err.message);
+              }
+              db.run(deleteTableQuery, () => {
+                db.close();
+              });
+            },
+          );
+        });
+      },
+    );
+  });
+};
+
+registerDatabaseWithErrors(db);
