40ecc8750fee9a6eae42b7d2eb0a836eceb2c589
yumiya <52166120+yumiya-g@users.noreply.github.com>
2025-07-31 08:21:34 +0900

    fix: `Async`関数で「エラーなし」の処理を修正しました
    - `await`式を使って逐次的に`Promise`を処理

--- 03.asynchronous/async.js
+++ 03.asynchronous/async.js
@@ -16,16 +16,31 @@ import {
 
 async function runWithoutError() {
   const db = new sqlite3.Database(":memory:");
+  let result = await promiseRun(db, createTableQuery);
+
+  result = await promiseRun(
+    db,
+    insertTableQuery,
+    "スラスラ読める JavaScriptふりがなプログラミング",
+  );
+  console.log(`ID: ${result.lastID}`);
+
+  result = await promiseRun(db, insertTableQuery, "初めてのJavaScript");
+  console.log(`ID: ${result.lastID}`);
+
+  await promiseEach(db, selectTableQuery, (_err, row) => {
+    console.log(`ID: ${row.id}, タイトル: ${row.title}`);
+  });
+  await promiseRun(db, deleteTableQuery);
+  await promiseClose(db);
 }
 
-
-async function runWithError() {
-  const db = new sqlite3.Database(":memory:");
-}
+// async function runWithError() {
+//   const db = new sqlite3.Database(":memory:");
+// }
 
 runWithoutError();
 
 await timers.setTimeout(100);
 
-runWithError();
-
+// runWithError();
