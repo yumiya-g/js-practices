bc90c7251ff9359060de4c0a4b0483db214ec1ca
yumiya <52166120+yumiya-g@users.noreply.github.com>
2025-07-30 12:56:34 +0900

    fix: Async/awaitのコードを`promise.js`に合わせて定義しなおしました
    - importの定義を変更
    - 関数名を`promise.js`と合わせて変更し、処理を削除
    - `sqlite3.Database`をインメモリデータベースとして関数内で定義

--- 03.asynchronous/async.js
+++ 03.asynchronous/async.js
@@ -1,69 +1,31 @@
 import timers from "timers/promises";
-import { db, promiseRun, promiseEach, recreateDB } from "./functions.js";
+import sqlite3 from "sqlite3";
 import {
-  firstBook,
-  secondBook,
-  firstBookWrong,
-  secondBookWrong,
-} from "./args.js";
-
+  promiseRun,
+  promiseEach,
+  promiseClose,
+} from "./promiseWrappedFunctions.js";
 import {
   createTableQuery,
+  insertTableQuery,
   selectTableQuery,
+  insertTableWrongQuery,
   selectTableWrongQuery,
   deleteTableQuery,
 } from "./queries.js";
 
-async function exportBookLists() {
-  const args = [firstBook, secondBook];
-  await promiseRun(createTableQuery);
-
-  for (const arg of args) {
-    try {
-      await promiseRun(arg.query, arg.params, arg.callback);
-    } catch (err) {
-      console.error(err.message);
-    }
-  }
-
-  await promiseEach(selectTableQuery, (_err, row) => {
-    console.log(`ID: ${row.id}, タイトル: ${row.title}`);
-  });
+async function runWithoutError() {
+  const db = new sqlite3.Database(":memory:");
 }
 
-exportBookLists().finally(() => {
-  db.run(deleteTableQuery, () => {
-    db.close();
-  });
-});
+
+async function runWithError() {
+  const db = new sqlite3.Database(":memory:");
+}
+
+runWithoutError();
 
 await timers.setTimeout(100);
 
-recreateDB();
+runWithError();
 
-async function exportErrBookLists() {
-  const args = [firstBookWrong, secondBookWrong];
-  await promiseRun(createTableQuery);
-
-  for (const arg of args) {
-    try {
-      await promiseRun(arg.query, arg.params, arg.callback);
-    } catch (err) {
-      console.error(err.message);
-    }
-  }
-
-  try {
-    await promiseEach(selectTableWrongQuery, (_err, row) => {
-      console.log(`ID: ${row.id}, タイトル: ${row.title}`);
-    });
-  } catch (err) {
-    console.error(err.message);
-  }
-}
-
-exportErrBookLists().finally(() => {
-  db.run(deleteTableQuery, () => {
-    db.close();
-  });
-});
