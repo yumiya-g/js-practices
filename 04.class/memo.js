193abcb59ebb503a140b16dd1c446712c01ae0a9
yumiya <52166120+yumiya-g@users.noreply.github.com>
2025-10-12 15:22:48 +0900

    fix: OutputHanderを追加して`-l`オプションの機能を実装しました
    - `OutputHandler`クラスをインポート
    - 使用していない`Memo`クラスのインスタンス変数を削除
    - タイトルを出力する`showLists`関数を追加

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -10,45 +10,33 @@ import {
 } from "./promiseWrappedFunctions.js";
 
 import { InputHandler } from "./inputHandler.js";
+import { OutputHandler } from "./outputHandler.js";
 
 class Memo {
   constructor() {
     this.inputHandler = new InputHandler();
-
-    this.title = null;
-    this.text = null;
-    this.option = null;
+    this.outputHandler = new OutputHandler();
   }
 
   async readStdIn() {
     // オプションがなければ標準入力を受け付ける
-    console.log(`argv:${process.argv[2]}`)
     if (process.argv[2] === undefined) {
-      
       return await this.inputParse();
     } else {
       // オプションがあれば出力処理を始める
-      console.log("出力処理を開始します");
+      const memos = await this.outputHandler.outputMemos();
+      return { memos: memos, option: process.argv[2]};
     }
   }
 
-  async inputParse() {
-    const result = await this.inputHandler.parseStdIn();
-
-    this.title = result.title;
-    this.text = result.text;
-    this.option = result.option;
-    return { title: this.title, text: this.text, option: this.option };
-  }
-
-  async save() {
+  async save(memo) {
     const db = new sqlite3.Database("memos.db");
 
     try {
       // memosテーブルを作成
       await promiseRun(db, createTableQuery);
       // データベース登録を実行
-      await promiseRun(db, insertTableQuery, [this.title, this.text]);
+      await promiseRun(db, insertTableQuery, [memo.title, memo.text]);
     } catch (err) {
       console.error(err);
       throw err;
@@ -57,7 +45,11 @@ class Memo {
     }
   }
 
-  show() {}
+  showLists(parsedMemo) {
+    this.outputHandler.outputLists(parsedMemo.memos);
+  }
+
+  showContents() {}
 
   delete() {}
 }
@@ -66,16 +58,14 @@ async function main() {
   // メモインスタンスを作成
   const memo = new Memo();
 
-
   // 標準入力をパース
   const parsedMemo = await memo.readStdIn();
 
   // オプションなしで保存処理を開始
   if (parsedMemo.option === undefined) {
-    await memo.save();
+    await memo.save(parsedMemo);
   } else if (parsedMemo.option === "-l") {
-    console.log("タイトル一覧を表示する");
-    // オプション`l`：データベースからタイトル一覧を取得する
+    await memo.showLists(parsedMemo);
   } else if (parsedMemo.option === "-r") {
     console.log("タイトル一覧を表示して本文を出力する");
   } else if (parsedMemo.option === "-d") {
