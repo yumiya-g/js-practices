9f3ff24711e61a4649ad361b35489b57149ee0a1
yumiya <52166120+yumiya-g@users.noreply.github.com>
2025-11-01 21:41:34 +0900

    fix: `MemoController`クラス作成による、他クラスの調整を行いました
    - `Memo`クラスが不要になったので削除
    - `OutputHandler`クラスが持っていた分岐処理を削除

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,59 +1,10 @@
 #!/usr/bin/env node
 
-import { InputHandler } from "./inputHandler.js";
-import { OutputHandler } from "./outputHandler.js";
-import { MemoRepositry } from "./memoRepositry.js";
-
-class Memo {
-  constructor() {
-    this.inputHandler = new InputHandler();
-    this.outputHandler = new OutputHandler();
-    this.memoRepositry = new MemoRepositry();
-  }
-
-  async readStdIn() {
-    const stdinOption = process.argv[2];
-    // オプションがなければ標準入力を受け付ける
-    if (stdinOption === undefined) {
-      return await this.inputHandler.parseStdIn();
-    } else {
-      // オプションがあれば出力処理を始める
-      const memos = await this.memoRepositry.find();
-      return { memos, option: stdinOption };
-    }
-  }
-
-  showLists(parsedMemo) {
-    this.outputHandler.outputLists(parsedMemo.memos);
-  }
-
-  async selectMemos(parsedMemo) {
-    await this.outputHandler.outputContents(parsedMemo);
-  }
-}
+import { MemoController } from "./memoController.js";
 
 async function main() {
-  // メモインスタンスを作成
-  const memo = new Memo();
-
-  // メモリポジトリインスタンスを作成
-  const memoRepositry = new MemoRepositry();
-
-  // 標準入力をパース
-  const parsedMemo = await memo.readStdIn();
-
-  // オプションなしで保存処理を開始
-  if (parsedMemo.option === undefined) {
-    await memoRepositry.save(parsedMemo);
-  } else if (parsedMemo.option === "-l") {
-    await memo.showLists(parsedMemo);
-  } else if (parsedMemo.option === "-r" || parsedMemo.option === "-d") {
-    await memo.selectMemos(parsedMemo);
-  } else {
-    console.log("存在しないオプションが入力されました");
-  }
-
-  // プロセスを終了する
+  const memoController = new MemoController();
+  await memoController.run();
   process.exit(0);
 }
 
--- 04.class/outputHandler.js
+++ 04.class/outputHandler.js
@@ -1,60 +1,39 @@
 import enquirer from "enquirer";
-import { MemoRepositry } from "./memoRepositry.js";
-
 const { Select } = enquirer;
 
 export class OutputHandler {
   outputLists(memos) {
     if (memos.length === 0) {
       console.log("登録されたメモはありません");
-    } else {
-      for (const memo of memos) {
-        console.log(memo.title);
-      }
+      return;
+    }
+    for (const memo of memos) {
+      console.log(memo.title);
     }
   }
 
-  async outputContents(memos) {
-    if (memos.memos.length === 0) {
+  async selectMemo(memos, message) {
+    if (memos.length === 0) {
       console.log("登録されたメモはありません");
       return;
     }
 
     const selectOptions = [];
-    for (const memo of memos.memos) {
+    for (const memo of memos) {
       selectOptions.push({
         name: memo.title,
         value: memo,
       });
     }
 
-    const optionsDescription = {
-      "-r": "Choose a note you want to see:",
-      "-d": "Choose a memo you want to delete:",
-    };
-
-    try {
-      const prompt = new Select({
-        name: "Memos",
-        message: optionsDescription[memos.option],
-        choices: selectOptions,
-        result() {
-          return this.focused.value;
-        },
-      });
-
-      const answer = await prompt.run();
-      if (memos.option === "-r") {
-        console.log(answer.contents);
-      } else if (memos.option === "-d") {
-        // メモリポジトリインスタンスを作成
-        const memoRepositry = new MemoRepositry();
-        await memoRepositry.delete(answer.id);
-        console.log("選択したメモを削除しました");
-      }
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
+    const prompt = new Select({
+      name: "Memos",
+      message: message,
+      choices: selectOptions,
+      result() {
+        return this.focused.value;
+      },
+    });
+    return await prompt.run();
   }
 }
